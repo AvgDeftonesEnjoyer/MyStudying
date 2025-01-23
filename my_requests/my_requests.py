ea417d5225479b57f430dea64edaaa95f127c812
Shapka Oleksii <shapkaoleksiiu@gmail.com>
2025-01-23 11:44:03 +0200

    my_requests: add get method

--- my_requests/my_requests.py
+++ my_requests/my_requests.py
@@ -1,5 +1,13 @@
-try:
+check = int(input("Choose homework number: 1, 2, 3: "))
+
+if check == 1: # Використовуй бібліотеку requests, щоб зробити HTTP-запит до будь-якого публічного API і вивести отримані дані.    
     import requests
-    print("Requests module is installed!")
-except ModuleNotFoundError:
-    print("Requests module is NOT installed!")
+
+    url = 'https://loripsum.net/api/1/short/plaintext'
+
+    responce = requests.get(url)
+
+    with open('my_requests/story.txt', 'w') as file:
+        file.write(responce.text)
+
+    print(responce.status_code)
